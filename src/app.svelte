b7e48690c0215474d5e2e6b3f1e3695fe2c2b9e1
Gerald <gera2ld@live.com>
2022-07-16 01:35:27 +0800

    fix: migrate to typescript

--- src/app.svelte
+++ src/app.svelte
@@ -1,44 +1,34 @@
-<script>
-const targets = [
-  {
-    label: '上证A股',
-    value: 'SH',
-  },
-  {
-    label: '深证A股',
-    value: 'SZ',
-  },
-];
-const input = {
-  target: targets[0].value,
-  isBuying: true,
-  price: '0.00',
-  amount: 0,
-  commissionRate: 0.02,
-  minFee: 5,
-  isETF: false,
-};
-const output = {
-  commission: 0,
-  exchangeFee: 0,
-  stampDuty: 0,
-  transferFee: 0,
-  fee: 0,
-};
-$: {
-  const total = input.price * input.amount * 100;
-  const commission = total * input.commissionRate / 100;
-  const exchangeFee = total * 0.00687 / 100;
-  const baseFee = Math.max(input.minFee, commission + exchangeFee);
-  const stampDuty = input.isBuying || input.isETF ? 0 : total * 0.001;
-  const transferFee = input.target === 'SH' && !input.isETF ? total * 0.00001 : 0;
-  const fee = baseFee + stampDuty + transferFee;
-  output.commission = commission.toFixed(2);
-  output.exchangeFee = exchangeFee.toFixed(2);
-  output.stampDuty = stampDuty.toFixed(2);
-  output.transferFee = transferFee.toFixed(2);
-  output.fee = fee.toFixed(2);
-}
+<script lang="ts">
+  import { getFees } from './util';
+
+  const markets = [
+    {
+      label: '上证A股',
+      value: 'SH',
+    },
+    {
+      label: '深证A股',
+      value: 'SZ',
+    },
+  ];
+  const input = {
+    market: markets[0].value,
+    isBuying: true,
+    price: '0.00',
+    amount: 0,
+    commissionRate: 0.02,
+    minFee: 5,
+    isETF: false,
+  };
+  $: fees = getFees({
+    price: +input.price,
+    amount: +input.amount,
+    commissionRate: +input.commissionRate / 100,
+    minFee: +input.minFee,
+    side: input.isBuying ? 'buy' : 'sell',
+    market: input.market,
+    isETF: input.isETF,
+  });
 </script>
 
 <div class="max-w-screen-sm mx-auto px-4 flex flex-col items-stretch">
@@ -46,7 +36,9 @@ $: {
     <h1 class="text-3xl">股票交易手续费</h1>
 
     <a class="ml-4" href="https://github.com/intellilab/calc">
-      <img src="https://img.shields.io/github/stars/intellilab/calc?style=social" alt="tw-prism">
+      <img
+        src="https://img.shields.io/github/stars/intellilab/calc?style=social"
+      />
     </a>
   </header>
 
@@ -55,8 +47,8 @@ $: {
       <div class="label">交易对象：</div>
       <div class="flex">
         <select bind:value={input.target}>
-          {#each targets as target}
-          <option value={target.value}>{target.label}</option>
+          {#each markets as target}
+            <option value={target.value}>{target.label}</option>
           {/each}
         </select>
       </div>
@@ -64,32 +56,34 @@ $: {
     <div class="row">
       <div class="label">交易类型：</div>
       <div class="flex items-center">
-        <label class="mr-2"><input type="checkbox" bind:checked={input.isBuying}> 买入</label>
-        <label><input type="checkbox" bind:checked={input.isETF}> ETF</label>
+        <label class="mr-2"
+          ><input type="checkbox" bind:checked={input.isBuying} /> 买入</label
+        >
+        <label><input type="checkbox" bind:checked={input.isETF} /> ETF</label>
       </div>
     </div>
     <div class="row">
       <div class="label">成交价：</div>
       <div class="flex">
-        <input type="number" bind:value={input.price} step="0.01">元
+        <input type="number" bind:value={input.price} step="0.01" />元
       </div>
     </div>
     <div class="row">
       <div class="label">成交量：</div>
       <div class="flex">
-        <input type="number" bind:value={input.amount}>手
+        <input type="number" bind:value={input.amount} />手
       </div>
     </div>
     <div class="row">
       <div class="label">佣金：</div>
       <div class="flex">
-        <input type="number" bind:value={input.commissionRate} step="0.01">%
+        <input type="number" bind:value={input.commissionRate} step="0.01" />%
       </div>
     </div>
     <div class="row">
       <div class="label">最低佣金：</div>
       <div class="flex">
-        <input type="number" bind:value={input.minFee}>元
+        <input type="number" bind:value={input.minFee} />元
       </div>
     </div>
   </section>
@@ -97,39 +91,47 @@ $: {
   <section>
     <div class="row">
       <div class="label">总费用：</div>
-      <div>{output.fee}</div>元
+      <div>{fees.totalFee.toFixed(2)}</div>
+      元
     </div>
     <div class="row">
       <div class="label">佣金：</div>
       <div>
-        <span>{output.commission}</span>元
+        <span>{fees.commission.toFixed(2)}</span>元
       </div>
     </div>
     <div class="row">
       <div class="label">交易所规费：</div>
       <div>
-        <span>{output.exchangeFee}</span>元
+        <span>{fees.exchangeFee.toFixed(2)}</span>元
+        <div class="hint">（0.00687%）</div>
       </div>
     </div>
     <div class="row">
       <div class="label">印花税：</div>
       <div>
-        <span>{output.stampDuty}</span>元
+        <span>{fees.stampDuty.toFixed(2)}</span>元
         {#if !input.isBuying && !input.isETF}
-        <div class="hint">（卖出股票时征收印花税）</div>
+          <div class="hint">（卖出股票时征收印花税 0.1%）</div>
         {/if}
       </div>
     </div>
     <div class="row">
       <div class="label">过户费：</div>
       <div>
-        <span>{output.transferFee}</span>元
+        <span>{fees.transferFee.toFixed(2)}</span>元
         {#if input.target === 'SH' && !input.isETF}
-        <div class="hint">（上证股票交易收过户费）</div>
+          <div class="hint">（上证股票交易收过户费 0.001%）</div>
         {/if}
       </div>
     </div>
   </section>
 
-  <footer>Designed by <a href="https://gera2ld.space/" target="_blank" rel="noopener noreferrer">Gerald</a> with ❤️</footer>
+  <footer>
+    Designed by <a
+      href="https://gera2ld.space/"
+      target="_blank"
+      rel="noopener">Gerald</a
+    > with ❤️
+  </footer>
 </div>
---
+++ src/util.ts
@@ -0,0 +1,24 @@
+export function getFees(input: {
+  price: number;
+  amount: number;
+  commissionRate: number;
+  minFee: number;
+  side: 'buy' | 'sell';
+  market: 'SH' | 'SZ';
+  isETF: boolean;
+}) {
+  const total = input.price * input.amount * 100;
+  const commission = total * input.commissionRate;
+  const exchangeFee = total * 0.0000687;
+  const baseFee = Math.max(input.minFee, commission);
+  const stampDuty = input.side === 'buy' || input.isETF ? 0 : total * 0.001;
+  const transferFee = input.market === 'SH' && !input.isETF ? total * 0.00001 : 0;
+  const totalFee = baseFee + stampDuty + transferFee;
+  return {
+    commission,
+    exchangeFee,
+    stampDuty,
+    transferFee,
+    totalFee,
+  };
+}
